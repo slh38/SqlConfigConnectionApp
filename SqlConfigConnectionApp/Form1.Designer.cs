e98e746356a5a8c62ac5e152173115cbc91a9071
salih kocamaz <slh-38@hotmail.com>
2022-06-07 23:25:24 +0300

    App Create

--- SqlConfigConnectionApp/Form1.Designer.cs
+++ SqlConfigConnectionApp/Form1.Designer.cs
@@ -30,9 +30,10 @@ namespace SqlConfigConnectionApp
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxParola = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,41 +41,51 @@ namespace SqlConfigConnectionApp
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(26, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(83, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "KULLANICI ADI";
             // 
-            // textBox1
+            // textBoxKullaniciAdi
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(117, 13);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(132, 20);
+            this.textBoxKullaniciAdi.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(26, 39);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "label1";
+            this.label2.Text = "PAROLA ";
             // 
-            // textBox2
+            // textBoxParola
             // 
-            this.textBox2.Location = new System.Drawing.Point(112, 39);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 20);
-            this.textBox2.TabIndex = 1;
+            this.textBoxParola.Location = new System.Drawing.Point(117, 39);
+            this.textBoxParola.Name = "textBoxParola";
+            this.textBoxParola.Size = new System.Drawing.Size(132, 20);
+            this.textBoxParola.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GİRİŞ";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 160);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(291, 139);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxParola);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxKullaniciAdi);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -86,9 +97,10 @@ namespace SqlConfigConnectionApp
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxParola;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ SqlConfigConnectionApp/Telefon Rehberi.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace SqlConfigConnectionApp
+{
+    partial class Telefon_Rehberi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewKisiler = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDowTel = new System.Windows.Forms.NumericUpDown();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.labelKisiSAyisi = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKisiler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowTel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonDelete);
+            this.groupBox1.Controls.Add(this.buttonUpdate);
+            this.groupBox1.Controls.Add(this.buttonInsert);
+            this.groupBox1.Controls.Add(this.numericUpDowTel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxSoyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxAdi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 302);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridViewKisiler);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(570, 382);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // dataGridViewKisiler
+            // 
+            this.dataGridViewKisiler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKisiler.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewKisiler.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewKisiler.Name = "dataGridViewKisiler";
+            this.dataGridViewKisiler.Size = new System.Drawing.Size(564, 363);
+            this.dataGridViewKisiler.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADI";
+            // 
+            // textBoxAdi
+            // 
+            this.textBoxAdi.Location = new System.Drawing.Point(22, 37);
+            this.textBoxAdi.Name = "textBoxAdi";
+            this.textBoxAdi.Size = new System.Drawing.Size(156, 20);
+            this.textBoxAdi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SOYADI";
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.Location = new System.Drawing.Point(22, 86);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(156, 20);
+            this.textBoxSoyad.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "TEL";
+            // 
+            // numericUpDowTel
+            // 
+            this.numericUpDowTel.Location = new System.Drawing.Point(22, 141);
+            this.numericUpDowTel.Name = "numericUpDowTel";
+            this.numericUpDowTel.Size = new System.Drawing.Size(156, 20);
+            this.numericUpDowTel.TabIndex = 2;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(22, 188);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(156, 30);
+            this.buttonInsert.TabIndex = 3;
+            this.buttonInsert.Text = "EKLE";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(22, 224);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(156, 30);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "GÜNCELLE";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(22, 260);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(156, 30);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "SİL";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 320);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(199, 54);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "buttonExport";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(199, 54);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "buttonImport";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // labelKisiSAyisi
+            // 
+            this.labelKisiSAyisi.AutoSize = true;
+            this.labelKisiSAyisi.Location = new System.Drawing.Point(231, 412);
+            this.labelKisiSAyisi.Name = "labelKisiSAyisi";
+            this.labelKisiSAyisi.Size = new System.Drawing.Size(117, 13);
+            this.labelKisiSAyisi.TabIndex = 3;
+            this.labelKisiSAyisi.Text = "TOPLAM KİŞİ SAYISI: ";
+            // 
+            // Telefon_Rehberi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelKisiSAyisi);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Telefon_Rehberi";
+            this.Text = "Telefon_Rehberi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKisiler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowTel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.NumericUpDown numericUpDowTel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewKisiler;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label labelKisiSAyisi;
+    }
+}
